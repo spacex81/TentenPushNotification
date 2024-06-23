e8e8e713f89639538239c3b9f444de151a1bb241
YK <spacex81@gmail.com>
2024-06-23 16:05:37 +0900

    d

---
+++ PushNotification/PushNotification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushNotification
+//
+//  Created by jo on 6/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PushNotification/PushNotification/PushNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationApp.swift
+//  PushNotification
+//
+//  Created by jo on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
